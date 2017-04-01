6a43fde8556bfac8f751e8927ce2e1a7e5c9b76b
Camille Harris <Camille Harris>
2017-04-01 05:54:36 -0700

    add user

--- user.py
+++ user.py
@@ -1,1 +1,8 @@
-user class 
+interests = {"Outdoors": False, "Restaurants": False, "Art": False, "Landmarks and Historical Sites": False, "Shopping": False}
+
+#likes is an array of catagories that exist in intersts. 
+def newUser(self, likes):
+	for l in likes:
+		self.interests(l) = True
+
+
